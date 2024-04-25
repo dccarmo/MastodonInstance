898a2399512610c4ebf000c37ceeb1af41a2d2db
Diogo Carmo <>
2024-04-24 21:47:23 -0300

    Reproducible issue when initializing Masto (AbortSignal.timeout is not a function)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createRestAPIClient } from "masto";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    try {
+      const client = createRestAPIClient({
+        url: `https://mastodon.social/api/v1`,
+        accessToken: "",
+      });
+
+      client.v1.accounts.verifyCredentials().then(console.log).catch(console.log);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
